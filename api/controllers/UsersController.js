fc265c91506bc92ffe935684b29bcde7b80d2f38
unknown <rafanetow@gmail.com>
2015-12-27 14:35:39 -0600

    fixed some error of syntax on UserController

--- api/controllers/UsersController.js
+++ api/controllers/UsersController.js
@@ -1,18 +1,16 @@
-
-		'use strict';
-
 	angular.module('app').controller('UsersController', ['$scope','UserService', function($scope) {
+	  		  		$scope.user = '';
 	  		  		$scope.response = '';
 
-
-	  		  UserService.getUser($scope.user).
+ 			$scope.FindUser = function(){
+	  		 UserService.getUser($scope.user).
                 success(function(data, status, headers, config) {
            						$scope.response = data;
-
-
                 }).
                 error(function(data, status, headers, config) {
                   				 $scope.response = data;
+             });
+           };
 	}]);
 
 
