76e864424fe30118569fdc6c3c5a4843eac00fd4
unknown <rafanetow@gmail.com>
2015-12-27 14:26:11 -0600

    Modificated UserService and UserController

--- api/controllers/UsersController.js
+++ api/controllers/UsersController.js
@@ -1,55 +1,25 @@
-/**
- * UsersController
- *
- * @description :: Server-side logic for managing Users
- * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
- */
+
+		'use strict';
+
+	angular.module('app').controller('UsersController', ['$scope','UserService', function($scope) {
+	  		  		$scope.response = '';
 
 
- module.exports = {
+	  		  UserService.getUser($scope.user).
+                success(function(data, status, headers, config) {
+           						$scope.response = data;
 
-		hi: function (req, res) {
-			re
-				return res.send("Hi there!");
-		},
-		bye: function (req, res) {
-				return res.redirect("http://www.sayonara.com");
-		},
-	    findUser: function(req, res){
-			var userId = req.param("userId");
-				if (!req.param('userId'))
-     				res.badRequest('Sorry, you need to tell us the ID');
-				Users.findOne({id: userId}, function (err, user){
-				if (err) {
-					console.log(err);
-					return res.json(err);
-				}
-                 if(!user)
-                 	return res.json({success: false, msg: "User not found!"});
-				else{
-					console.log('Usuario:', user);
-					return res.json(user);
 
-				}
-			});
-		},
-		findAllUserWithAgeMayorToEighteen: function(req,res){
-     		Users.find({ age: { '>': 18 }}, function (err, user){
-     			if (err) {
-					console.log(err);
-					return res.json(err);
-				} if(!user)
-                 	return res.json({success: false, msg: "User not found!"});
-                 	return res.json(user);
-
-     		});
-
-		}
+                }).
+                error(function(data, status, headers, config) {
+                  				 $scope.response = data;
+	}]);
 
 
 
 
 
 
-};
+
+
 
